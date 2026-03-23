0cef3e7790bc906019d902099a25431dd9472ad2
Alberto-Neo <neoalberto15@gmail.com>
2026-03-23 00:21:13 -0300

    add controller

---
+++ src/main/java/Gerenciamento/Fornecedores/Controller/FornecedorController.java
@@ -0,0 +1,38 @@
+package Gerenciamento.Fornecedores.Controller;
+
+import Gerenciamento.Fornecedores.Models.FornecedorModel;
+import Gerenciamento.Fornecedores.Service.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fonecedores")
+public class FornecedorController {
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @GetMapping
+    public List<FornecedorModel> Lista(){
+        return fornecedorService.Listar();
+    }
+
+    @GetMapping("/{id}")
+    public FornecedorModel Buscar(long id){
+        return fornecedorService.Buscar(id);
+    }
+
+    @PostMapping
+    public FornecedorModel Criar(FornecedorModel fornecedorModel){
+        return fornecedorService.Criar(fornecedorModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> Deletar(long id){
+        fornecedorService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
